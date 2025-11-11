9539f3a054ae7f703620bd28ca65e8ad6a3e67a7
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-11-11 01:50:06 +0000

    Redis feature

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
@@ -30,4 +30,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
