7fea5030c7897f7fe22d196803cedaa355146013
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-11-13 21:30:13 +0000

    Created add a listing function

---
+++ backend/src/routes/listings.js
@@ -0,0 +1,6 @@
+// import express from 'express';
+// const router = express.Router();
+
+// import listingController from '../controllers/listingController.js';
+// import { protect} from '../middleware/auth.js';
+// import { checkListingLimit } from '../middleware/tierGuard.js';
